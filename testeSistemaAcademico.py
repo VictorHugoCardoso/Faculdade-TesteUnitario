77c77a8f69283426d142ffc5a339860a77c346d6
note <victor.mendes056@gmail.com>
2019-11-04 15:01:46 -0300

    d

--- testeSistemaAcademico.py
+++ testeSistemaAcademico.py
@@ -12,21 +12,46 @@ class sistemaAcademico(unittest.TestCase):
         cls.driver.maximize_window()
 
     def test_addAluno(self):
-        self.driver.get("http://localhost:8000/alunos/")
+        self.driver.get("http://trabsonjenkins.herokuapp.com/alunos/")
         self.driver.find_element_by_name("newaluno").click()
         self.driver.find_element_by_name("nomeAluno").send_keys("blablabla")
         self.driver.find_element_by_name("savebutton").click()
 
     def test_editAluno(self):
-        self.driver.get("http://localhost:8000/alunos/")
+        self.driver.get("http://trabsonjenkins.herokuapp.com/alunos/")
         self.driver.find_element_by_name("editaluno").click()
         self.driver.find_element_by_name("nomeAluno").send_keys("blebleble")
         self.driver.find_element_by_name("savebutton").click() 
 
     def test_removeAluno(self):
-        self.driver.get("http://localhost:8000/alunos/")
+        self.driver.get("http://trabsonjenkins.herokuapp.com/")
         self.driver.find_element_by_name("removealuno").click()
-        self.driver.find_element_by_name("deletealuno").click()       
+        self.driver.find_element_by_name("deletealuno").click()   
+
+    def test_addDisciplina(self):
+        self.driver.get("http://trabsonjenkins.herokuapp.com/disciplinas/")
+        self.driver.find_element_by_name("newdisciplina").click()
+        self.driver.find_element_by_name("nomeDisciplina").send_keys("Banco de dados")
+        self.driver.find_element_by_name("savebutton").click()     
+
+    def test_editDisciplina(self):
+        self.driver.get("http://trabsonjenkins.herokuapp.com/disciplinas/")
+        self.driver.find_element_by_name("editdisciplina").click()
+        self.driver.find_element_by_name("nomeDisciplina").send_keys("plus")
+        self.driver.find_element_by_name("savebutton").click()   
+
+    def test_editNota(self):
+        self.driver.get("http://trabsonjenkins.herokuapp.com/disciplinas/")
+        self.driver.find_element_by_name("editnota").click()
+        self.driver.find_element_by_name("editnotaaluno").click()
+        self.driver.find_element_by_name("nota2").send_keys("5")
+        self.driver.find_element_by_name("savebutton").click()
+
+    def test_addPresenca(self):
+        self.driver.get("http://trabsonjenkins.herokuapp.com/disciplinas/")
+        self.driver.find_element_by_name("namedisciplina").click()
+        self.driver.find_element_by_name("addpresenca").click()
+        self.driver.find_element_by_name("backbutton").click()              
 
     @classmethod
     def tearDownClass(cls):
